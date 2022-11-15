1c93d33f70da12039b9ee3929560cf60b092eafc
ykazarinov <kazarinov.yanek@gmail.com>
2022-11-15 15:12:39 +0100

    useRef for focus of search input

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -7,6 +7,12 @@ import { useSelector, useDispatch } from 'react-redux';
 export const Search = () => {
   const searchValue = useSelector((state) => state.search.searchValue);
   const dispatch = useDispatch();
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    dispatch(setSearchValue(''));
+    inputRef.current.focus();
+  };
 
   return (
     <div className={styles.root}>
@@ -21,13 +27,14 @@ export const Search = () => {
         <path d="M448.3,424.7L335,311.3c20.8-26,33.3-59.1,33.3-95.1c0-84.1-68.1-152.2-152-152.2c-84,0-152,68.2-152,152.2  s68.1,152.2,152,152.2c36.2,0,69.4-12.7,95.5-33.8L425,448L448.3,424.7z M120.1,312.6c-25.7-25.7-39.8-59.9-39.8-96.3  s14.2-70.6,39.8-96.3S180,80,216.3,80c36.3,0,70.5,14.2,96.2,39.9s39.8,59.9,39.8,96.3s-14.2,70.6-39.8,96.3  c-25.7,25.7-59.9,39.9-96.2,39.9C180,352.5,145.8,338.3,120.1,312.6z" />
       </svg>
       <input
+        ref={inputRef}
         value={searchValue}
         placeholder="Recherche de pizzas"
         onInput={(event) => dispatch(setSearchValue(event.target.value))}
       />
       {searchValue && (
         <svg
-          onClick={() => dispatch(setSearchValue(''))}
+          onClick={() => onClickClear()}
           className={styles.clearIcon}
           height="14px"
           version="1.1"
@@ -37,7 +44,7 @@ export const Search = () => {
           <title />
           <desc />
           <defs />
-          <g fill="none" fill-rule="evenodd" id="Page-1" stroke="none" stroke-width="1">
+          <g fill="none" fillRule="evenodd" id="Page-1" stroke="none" strokeWidth="1">
             <g fill="#000000" id="Core" transform="translate(-341.000000, -89.000000)">
               <g id="close" transform="translate(341.000000, 89.000000)">
                 <path
