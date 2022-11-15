8e76387b5a0fb25427a2ac4c2aef45151ebffd97
ykazarinov <kazarinov.yanek@gmail.com>
2022-11-15 16:39:32 +0100

    forcePage for pagination

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './Pagination.module.scss';
 import ReactPaginate from 'react-paginate';
+import { useSelector } from 'react-redux';
 
 export const Pagination = ({ onChangePage }) => {
+  const currentPage = useSelector((state) => state.pagination.currentPage);
   return (
     <ReactPaginate
       className={styles.root}
@@ -11,6 +13,7 @@ export const Pagination = ({ onChangePage }) => {
       onPageChange={(event) => onChangePage(event.selected + 1)}
       pageRangeDisplayed={4}
       pageCount={3}
+      forcePage={currentPage - 1}
       previousLabel="<"
       renderOnZeroPageCount={null}
     />
