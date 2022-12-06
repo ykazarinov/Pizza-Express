e4a6b3af0b9aea8d2352e00fa0b48d670b46e7d4
ykazarinov <kazarinov.yanek@gmail.com>
2022-12-06 17:35:44 +0100

    favicon

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,6 +6,7 @@ import Footer from '../components/Footer';
 import { selectActualLang } from '../redux/lang/selectors';
 
 import { LangEnum } from '../redux/lang/types';
+import { Helmet } from 'react-helmet';
 
 const MainLayout: React.FC = () => {
   const actualLang = useSelector(selectActualLang);
@@ -24,6 +25,15 @@ const MainLayout: React.FC = () => {
   return (
     <>
       <div className="wrapper">
+        <Helmet>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Helmet>
         <Header />
 
         <div className="content">
